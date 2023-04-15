c5245d4dd23e82e1493303d388e6653d0e8ec013
nithinf <118965903+nithinf@users.noreply.github.com>
2023-04-15 13:31:01 +0530

    Update main.tf

--- pavani/2-standard-build/1-Example/ec2-creation/main.tf
+++ pavani/2-standard-build/1-Example/ec2-creation/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2_instance" {
-    ami = "ami-0b0dcb5067f052a63"
+    ami = "ami-0263e4deb427da90e"
     instance_type = "t2.micro"
     vpc_security_group_ids = [var.SG]
     tags = {Name = "HTTP_SERVER"}
